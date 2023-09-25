e831617c62436f299384936917f45318630813db
Brooke Sauro <bhsauro@gmail.com>
2023-09-25 10:28:16 -0400

    completed basic form structure

--- client/App.jsx
+++ client/App.jsx
@@ -3,15 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header.jsx';
 import Card from './components/Card.jsx';
+import Onboarding from './pages/Onboarding.jsx'
 
 const App = () => {
     return (
         <>
             <Header />
-            <Card title='test' />
-            <div>
-                hi is this from react
-            </div>
+            <Onboarding />
         </>
     )
 }
--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 
-
-
 export default function Card(props) { 
     return (
         <div className="card">
