d6e7f1b88fc3d31cf3da36dbdfa41c0d3dcb7e6e
Katya Villano <katyavillano@Katyas-MacBook-Pro.local>
2023-09-25 14:46:48 -0400

    added signup and login components

--- client/App.jsx
+++ client/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Onboarding from './pages/Onboarding.jsx'
 import Home from "./pages/Home.jsx";
+import Signup from "./components/Signup.jsx";
+import Login from "./components/Login.jsx"
 
 const App = () => {
     return (
